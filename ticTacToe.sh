c2af88f7bbea87f9c720d78b4a87ece567aeeb3c
rajkush211 <rajkush211.rk@gmail.com>
2020-03-26 05:28:15 +0530

    ADDED: Checking Winner, Tie, and changing turn

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -11,20 +11,28 @@ USER_LETTER=X
 COMP_LETTER=O
 USER_TURN=0
 COMP_TURN=1
+TRUE=1
+FALSE=0
+
+#VARIABLES
+isWon=0
+vacantCells=9
 
 declare -A Board
 
 function resetBoard() {
+	local count=0
 	for (( i=0; i<3; i++ ))
 	do
-		echo "---------------"
+		echo "---------------------"
 		for (( j=0; j<3; j++ ))
 		do
-			echo -n "|   |"
+			Board[$i,$j]=$(( count++ ))
+			echo -n "|  ${Board[$i,$j]}  |"
 		done
 		echo
 	done
-	echo "---------------"
+	echo "---------------------"
 }
 
 function tossForTurn() {
@@ -38,21 +46,71 @@ function tossForTurn() {
 }
 
 function displayBoard() {
-	count=0
 	for (( i=0; i<3; i++ ))
 	do
-		echo "---------------"
+		echo "---------------------"
 		for (( j=0; j<3; j++ ))
 		do
-			echo -n "| $(( count++ )) |"
+			echo -n "|  ${Board[$i,$j]}   |"
 		done
 		echo
 	done
-	echo "---------------"
+	echo "---------------------"
+}
+
+function areThreeCellsEqual() {
+	cell1=$1
+	cell2=$2
+	cell3=$3
+	if [ ! -z "$cell1" ]  #CHECKING IF CELL IS NON-EMPTY
+	then
+		if [[ $cell1 == $cell2 && $cell2 == $cell3 ]]
+		then
+			isWon=$TRUE
+			if [ $cell1 == $USER_LETTER ]
+			then
+				winner=$USER_LETTER
+			else
+				winner=$COMP_LETTER
+			fi
+		fi
+	fi
+}
+
+function checkWinning() {
+	isWon=$FALSE
+#CHECKING EACH ROW AND COLUMN AT EACH ITERATION IF WON
+	for (( i=0; i<3; i++ ))
+	do
+		areThreeCellsEqual ${Board[$i,0]} ${Board[$i,1]} ${Board[$i,2]} #FOR ROW
+		areThreeCellsEqual ${Board[0,$i]} ${Board[1,$i]} ${Board[2,$i]} #FOR COLUMN
+	done
+#CHECKING BOTH DIAGONAL
+	areThreeCellsEqual ${Board[0,0]} ${Board[1,1]} ${Board[2,2]} #FOR 1 DIAGONAL
+	areThreeCellsEqual ${Board[0,2]} ${Board[1,1]} ${Board[2,0]} #FOR 2 DIAGONAL
+	if [ $isWon -eq $TRUE ]
+	then
+		if [ $winner == $USER_LETTER ]
+		then
+			echo "Congratulations, You won! well played."
+		elif [ $winner == $COMP_LETTER ]
+		then
+			echo "Computer won!, Better luck next time"
+		fi
+	else
+		if [[ $vacantCells -eq 0 ]] #TIE CONDITION IF THERE IS NO VACANT CELLS
+		then
+			echo "Game Tie"
+		else
+			turn=$(( 1 - turn ))
+		fi
+	fi
 }
 
 echo "You are assigned letter:- $USER_LETTER"
 tossForTurn
 displayBoard
+resetBoard
+checkWinning
 
 sleep 1
