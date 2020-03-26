4500e45b952f28920a6983e7612905fdcca4c98a
rajkush211 <rajkush211.rk@gmail.com>
2020-03-26 07:51:17 +0530

    ADDED: computer can check and play winning move

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -21,13 +21,12 @@ vacantCells=9
 declare -A Board
 
 function resetBoard() {
-	local count=0
 	for (( i=0; i<3; i++ ))
 	do
 		echo "---------------------"
 		for (( j=0; j<3; j++ ))
 		do
-			Board[$i,$j]=$(( count++ ))
+			Board[$i,$j]=" "
 			echo -n "|  ${Board[$i,$j]}  |"
 		done
 		echo
@@ -39,9 +38,11 @@ function tossForTurn() {
 	toss=$(( RANDOM % 2 ))
 	case $toss in
 		$USER_TURN)
-			echo "You won the toss, your turn first";;
+			echo "You won the toss, your turn first"
+			whoseTurn=$USER_TURN;;
 		$COMP_TURN)
-			echo "You loss the toss, computer turn first";;
+			echo "You loss the toss, computer turn first"
+			whoseTurn=$COMP_TURN;;
 	esac
 }
 
@@ -51,7 +52,7 @@ function displayBoard() {
 		echo "---------------------"
 		for (( j=0; j<3; j++ ))
 		do
-			echo -n "|  ${Board[$i,$j]}   |"
+			echo -n "|  ${Board[$i,$j]}  |"
 		done
 		echo
 	done
@@ -77,7 +78,7 @@ function areThreeCellsEqual() {
 	fi
 }
 
-function checkWinning() {
+function checkIfAnyoneWon() {
 	isWon=$FALSE
 #CHECKING EACH ROW AND COLUMN AT EACH ITERATION IF WON
 	for (( i=0; i<3; i++ ))
@@ -101,18 +102,115 @@ function checkWinning() {
 		if [[ $vacantCells -eq 0 ]] #TIE CONDITION IF THERE IS NO VACANT CELLS
 		then
 			echo "Game Tie"
-		else
-			turn=$(( 1 - turn ))
 		fi
 	fi
 }
 
-echo "You are assigned letter:- $USER_LETTER"
-tossForTurn
-displayBoard
-resetBoard
-checkWinning
+function checkCanCompWin() {
+	letter=$1
+	placeAtRow=""
+	placeAtColumn=""
+	for (( i=0; i<3; i++ ))
+	do
+#CHECKING WINNING IN ROWS
+		if [[ ${Board[$i,0]} == $letter && ${Board[$i,1]} == $letter && ${Board[$i,2]} == " " ]]
+		then
+			placeAtRow=$i
+			placeAtColumn=2
+			break
+		elif [[ ${Board[$i,0]} == $letter && ${Board[$i,2]} == $letter && ${Board[$i,1]} == " " ]]
+		then
+			placeAtRow=$i
+			placeAtColumn=1
+			break
+		elif [[ ${Board[$i,1]} == $letter && ${Board[$i,2]} == $letter && ${Board[$i,0]} == " " ]]
+		then
+			placeAtRow=$i
+			placeAtColumn=0
+			break
+#CHECKING WINNING IN COLUMNS
+		elif [[ ${Board[0,$i]} == $letter && ${Board[1,$i]} == $letter && ${Board[2,$i]} == " " ]]
+		then
+			placeAtRow=2
+			placeAtColumn=$i
+			break
+		elif [[ ${Board[0,$i]} == $letter && ${Board[2,$i]} == $letter && ${Board[1,$i]} == " " ]]
+		then
+			placeAtRow=1
+			placeAtColumn=$i
+			break
+		elif [[ ${Board[1,$i]} == $letter && ${Board[2,$i]} == $letter && ${Board[0,$i]} == " " ]]
+		then
+			placeAtRow=0
+			placeAtColumn=$i
+			break
+		fi
+	done
 
-echo "Computer is playing smart like you, So be focused"
+#CHECKING WINNING IN BOTH DIAGONALS
+	if [[ $placeAtRow == ""  && $placeAtColumn == "" ]]
+	then
+		if [[ ${Board[0,0]} == $letter && ${Board[1,1]} == $letter && ${Board[2,2]} == " " ]]
+		then
+			placeAtRow=2
+			placeAtColumn=2
+		elif [[ ${Board[0,0]} == $letter && ${Board[2,2]} == $letter && ${Board[1,1]} == " " ]]
+		then
+			placeAtRow=1
+			placeAtColumn=1
+		elif [[ ${Board[1,1]} == $letter && ${Board[2,2]} == $letter && ${Board[0,0]} == " " ]]
+		then
+			placeAtRow=0
+			placeAtColumn=0
+		elif [[ ${Board[0,2]} == $letter && ${Board[1,1]} == $letter && ${Board[2,0]} == " " ]]
+		then
+			placeAtRow=2
+			placeAtColumn=0
+		elif [[ ${Board[0,2]} == $letter && ${Board[2,0]} == $letter && ${Board[1,1]} == " " ]]
+		then
+			placeAtRow=1
+			placeAtColumn=1
+		elif [[ ${Board[1,1]} == $letter && ${Board[2,0]} == $letter && ${Board[0,2]} == " " ]]
+		then
+			placeAtRow=0
+			placeAtColumn=2
+		fi
+	fi
+}
+
+function playTicTacToe() {
+	echo "You are assigned letter:- $USER_LETTER"
+	tossForTurn
+	resetBoard
+	displayBoard
+	checkIfAnyoneWon
+	if [[ $isWon -eq $FALSE && $vacantCells -ne 0 ]]
+	then
+		if [ $whoseTurn -eq $USER_TURN ]
+		then
+			while true
+			do
+				read -p "Your turn to play enter row(0-2) and column(0-2): " row column
+				if [[ ${Board[$row,$column]} == " " ]]
+				then
+					Board[$row,$column]=$USER_LETTER
+					(( vacantCells-- ))
+					break
+				fi
+			done
+		else
+			echo "Computer's Turn"
+			checkCanCompWin $COMP_LETTER
+			if [[ $placeAtRow != "" && $placeAtColumn != "" ]]
+			then
+				Board[$placeAtRow,$placeAtColumn]=$COMP_LETTER
+			fi
+			(( vacantCells-- ))
+			whoseTurn=$(( 1 - whoseTurn ))
+		fi
+	fi
+}
+
+playTicTacToe
 
 sleep 1
